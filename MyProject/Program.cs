fee5e2ff10112450baea514028e31a9b93fb823e
Robson De Andrade <theairelibre@protonmail.com>
2022-07-11 11:14:58 -0300

    Initial files

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Automovel
+{
+    public class Program
+    {
+        static void Main()
+        {
+            IVeiculo moto = new Moto();
+            moto.NumeroChassi = "123MRERTXP20";
+
+            Console.WriteLine(moto.NumeroChassi);
+        }
+    }
+}
