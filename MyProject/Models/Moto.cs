bf645ce53453e8018dce9acbac421fdcfd6bd662
Robson De Andrade <theairelibre@protonmail.com>
2022-07-12 11:04:48 -0300

    Create constructor and arguments base class

--- MyProject/Models/Moto.cs
+++ MyProject/Models/Moto.cs
@@ -2,30 +2,15 @@ namespace Automovel
 {
     public class Moto : Veiculo
     {
-        private string _chassi;
-        private double _potencia;
-        private int _rodas;
-
-        public string NumeroChassi
-        {
-            get => _chassi;
-            set => _chassi = value;
-        }
-        public double Potencia
-        {
-            get => _potencia;
-            set => _potencia = value;
-        }
-        public int Rodas
-        {
-            get => _rodas;
-            set => _rodas = value;
-        }
-        public Moto()
-        {
-            _chassi = NumeroChassi;
-            _potencia = Potencia;
-            _rodas = Rodas;
-        }
+        public int _rodas = 2;
+        public Moto(
+            string chassi,
+            string placa,
+            string marca,
+            int data,
+            double potencia,
+            string cor,
+            decimal valor
+        ) : base(chassi, placa, marca, data, potencia, cor, valor) { }
     }
 }
