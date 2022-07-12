f192df4191063ba60de79a69c7f9ab8b365eec85
Robson De Andrade <theairelibre@protonmail.com>
2022-07-12 11:02:33 -0300

    Create attributes, constructor and method

--- MyProject/Models/Veiculo.cs
+++ MyProject/Models/Veiculo.cs
@@ -2,7 +2,36 @@ namespace Automovel
 {
     public class Veiculo
     {
-        string NumeroChassi { get; set; }
+        public string NumeroChassi { get; set; }
+        public string Placa { get; set; }
+        public string NomeMarca { get; set; }
+        public int DataFabricacao { get; set; }
+        public double Potencia { get; set; }
+        public string Cor { get; set; }
+        public decimal Valor { get; set; }
+        public int CPF { get; set; }
 
+        public Veiculo(
+            string chassi,
+            string placa,
+            string marca,
+            int data,
+            double potencia,
+            string cor,
+            decimal valor
+        )
+        {
+            NumeroChassi = chassi;
+            Placa = placa;
+            NomeMarca = marca;
+            DataFabricacao = data;
+            Potencia = potencia;
+            Cor = cor;
+            Valor = valor;
+        }
+        public void VenderVeiculos() 
+        { 
+
+        }
     }
 }
