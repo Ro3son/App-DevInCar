5e9e42ce3ec0436ee2554fb2a5a362cf47280831
Robson De Andrade <theairelibre@protonmail.com>
2022-07-19 15:45:09 -0300

    Remove method to base class

--- MyProject/Models/Moto.cs
+++ MyProject/Models/Moto.cs
@@ -12,27 +12,9 @@ namespace Automovel.Models
             int data,
             double potencia,
             string cor,
-            decimal valor
-        ) : base(chassi, placa, modelo, data, potencia, cor, valor) { }
-
-        public override void VenderVeiculos()
-        {
-            WriteLine("Deseja vender o veículo disponível? (s/n)");
-            var entrada = ReadLine();
-            if (entrada == "s")
-            {
-                WriteLine("Digite o ID(placa) do veículo: ");
-                var id = ReadLine();
-                WriteLine("Digite o CPF(somente números) do comprador: ");
-                var cpf = ReadLine();
-                WriteLine("Digite o valor do veículo: ");
-                var valor = ReadLine();
-                WriteLine("\n ---- Motos Vendidas ---- \n");
-                Console.ForegroundColor = ConsoleColor.Red;
-                WriteLine($"Placa: {id}, CPF comprador {cpf}, Valor: {valor} \n");
-                WriteLine($"Placa: BRA2S5, CPF comprador 11122233345, Valor: 12,600 \n");
-                ResetColor();
-            }
-        }
+            decimal valor,
+            ETipo tipo
+        ) : base(chassi, placa, modelo, data, potencia, cor, valor, tipo) { }
+        
     }
 }
