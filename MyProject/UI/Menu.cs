edb8e509992a1c6fb1875f77ef1c1b2b626e1acc
Robson De Andrade <theairelibre@protonmail.com>
2022-07-19 10:50:48 -0300

    Move method ExecutarPrograma to class Program

--- MyProject/UI/Menu.cs
+++ MyProject/UI/Menu.cs
@@ -1,4 +1,3 @@
-using Automovel.Models;
 using static System.Console;
 
 namespace Automovel.UI
@@ -23,65 +22,6 @@ namespace Automovel.UI
                 "0 - Sair\n"
             );
             ResetColor();
-
-        }
-
-        public static void ExecutarPrograma()
-        {
-            var condition = true;
-
-            while (condition)
-            {
-                int option = Convert.ToInt32(Console.ReadLine());
-
-                EMenu options = (EMenu)option;
-
-                switch (options)
-                {
-                    case EMenu.Motos:
-
-                        WriteLine("\n ---- Motos Disponíveis ---- \n");
-
-                        List<Moto> motos = new List<Moto>
-                        {
-                            new Moto(
-                                "CHASSI12454T4T456",
-                                "PLC123",
-                                "CG 160 Titan",
-                                2021,
-                                15.1,
-                                "Preto",
-                                12.000m
-                            ),
-                            new Moto(
-                                "CHASSI12454T4T456",
-                                "PLC123",
-                                "CG 160 Titan",
-                                2020,
-                                15.1,
-                                "Prata",
-                                8.000m
-                            ),
-                        };
-
-                        foreach (var moto in motos)
-                        {   
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                            WriteLine(
-                                $"Chassi: {moto.NumeroChassi}, Placa: {moto.Placa}, Modelo: {moto.NomeModelo}\n"
-                                    + $"Potência: {moto.Potencia} (cv),  Ano: {moto.DataFabricacao}, Cor: {moto.Cor}, Preço: {moto.Valor}\n"
-                            );
-                            ResetColor();
-
-                            moto.VenderVeiculos();
-                        }
-
-                        WriteLine("---- Motos vendidas com o maior preço ----");
-
-                        WriteLine("---- Motos vendidas com menor preço ----");
-                        break;
-                }
-            }
         }
     }
 }
