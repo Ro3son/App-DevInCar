e3fa832a25743a10befd934acc09b43c6ed8600a
Robson De Andrade <theairelibre@protonmail.com>
2022-07-19 15:46:44 -0300

    Create List

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -12,60 +12,51 @@ namespace Automovel
             Menu.MostrarMenu();
             ExecutarPrograma();
         }
+
         public static void ExecutarPrograma()
         {
-            var condition = true;
+            List<Veiculo> veiculos = new List<Veiculo>();
 
-            while (condition)
+            veiculos.Add(
+                new Moto("CHASSI12454T4T456", "PLC1234", "CG 160 Titan", 2022, 15.1, "Cinza", 14.620m,ETipo.Moto));
+            veiculos.Add(
+                new Moto("CHASSI123DFGEVGBU", "BRA2S5D", "CG 160 Fan", 2017, 15.1, "Azul", 13.480m, ETipo.Moto));
+            veiculos.Add(
+                new Moto("CHASSIQER234GYHJC", "CBA3S5K", "CG 160 Cargo", 2021, 15.1, "Branco", 13.650m, ETipo.Moto));
+            veiculos.Add(
+                new Moto("CHASSIYUIH78VR67J", "SPA1SL3", "CG 160 Start", 2019, 15.1, "Prata", 12.280m, ETipo.Moto));
+
+            while (true)
             {
-                int option = Convert.ToInt32(Console.ReadLine());
+                var entrada = Convert.ToInt32(ReadLine());
 
-                EMenu options = (EMenu)option;
+                ETipo options = (ETipo)entrada;
 
                 switch (options)
                 {
-                    case EMenu.Motos:
+                    case ETipo.Moto:
 
                         WriteLine("\n ---- Motos Disponíveis ---- \n");
 
-                        List<Moto> motos = new List<Moto>
-                        {
-                            new Moto(
-                                "CHASSI12454T4T456",
-                                "PLC123",
-                                "CG 160 Titan",
-                                2021,
-                                15.1,
-                                "Preto",
-                                12.000m
-                            ),
-                            new Moto(
-                                "CHASSI12454T4T456",
-                                "PLC123",
-                                "CG 160 Titan",
-                                2020,
-                                15.1,
-                                "Prata",
-                                8.000m
-                            ),
-                        };
+                        var motos =
+                            from veiculo in veiculos
+                            where veiculo.Tipo == ETipo.Moto
+                            select veiculo;
 
                         foreach (var moto in motos)
                         {
                             Console.ForegroundColor = ConsoleColor.DarkGreen;
                             WriteLine(
-                                $"Chassi: {moto.NumeroChassi}, Placa: {moto.Placa}, Modelo: {moto.NomeModelo}\n"
-                                    + $"Potência: {moto.Potencia} (cv),  Ano: {moto.DataFabricacao}, Cor: {moto.Cor}, Preço: {moto.Valor}\n"
+                                $"Chassi: {moto.NumeroChassi}, Placa: {moto.Placa}, Modelo: {moto.NomeModelo}\n" +
+                                $"Potência: {moto.Potencia} (cv), Ano: {moto.DataFabricacao}, Cor: {moto.Cor}, Preço: {moto.Valor}\n"
                             );
                             ResetColor();
-
-                            moto.VenderVeiculos();
                         }
 
                         WriteLine("---- Motos vendidas com o maior preço ----");
 
                         WriteLine("---- Motos vendidas com menor preço ----");
-                        break;
+                    break;
                 }
             }
         }
