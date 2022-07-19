aa4c64127a1dd8792c25e20088a875a37a55dd7d
Robson De Andrade <theairelibre@protonmail.com>
2022-07-19 10:46:17 -0300

    Add new option menu

--- MyProject/UI/Menu.cs
+++ MyProject/UI/Menu.cs
@@ -15,10 +15,14 @@ namespace Automovel.UI
                 "2 - Triciclo\n" +
                 "3 - Carros\n" +
                 "4 - Camionete\n" +
-                "5 - Todos\n" +
+                "5 - Todos\n"
+            );
+            WriteLine(
+                "Outras opções: \n" +
+                "6 - Histórico\n" +
                 "0 - Sair\n"
             );
-            Console.ResetColor();
+            ResetColor();
 
         }
 
@@ -67,7 +71,7 @@ namespace Automovel.UI
                                 $"Chassi: {moto.NumeroChassi}, Placa: {moto.Placa}, Modelo: {moto.NomeModelo}\n"
                                     + $"Potência: {moto.Potencia} (cv),  Ano: {moto.DataFabricacao}, Cor: {moto.Cor}, Preço: {moto.Valor}\n"
                             );
-                            Console.ResetColor();
+                            ResetColor();
 
                             moto.VenderVeiculos();
                         }
