81c8f3e1a9a189d2a502cbe887b038b3219b26a6
Robson De Andrade <theairelibre@protonmail.com>
2022-07-20 11:14:32 -0300

    Override method from base class

--- MyProject/Models/Moto.cs
+++ MyProject/Models/Moto.cs
@@ -15,6 +15,25 @@ namespace Automovel.Models
             decimal valor,
             ETipo tipo
         ) : base(chassi, placa, modelo, data, potencia, cor, valor, tipo) { }
+
+        public override void VenderVeiculos()
+        {
+            WriteLine("Deseja vender o veículo disponível? (s/n)");
+            var input = ReadLine();
+            if (input == "s")
+            {
+                WriteLine("Digite o ID(placa) do veículo: ");
+                var id = ReadLine();
+                WriteLine("Digite o CPF(somente números) do comprador: ");
+                var cpf = ReadLine();
+                WriteLine("Digite o valor do veículo: ");
+                var valor = ReadLine();
+                WriteLine("\n ---- Moto Vendida ---- \n");
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine($"Placa: {id}, CPF comprador {cpf}, Valor: {valor}, \n");
+                ResetColor();
+            }
+        }
         
     }
 }
