0cac06e98cd4bc675234a51a9ceee1c8179de469
Robson De Andrade <theairelibre@protonmail.com>
2022-07-20 11:16:22 -0300

    Add abstract method and regular methods

--- MyProject/Models/Veiculo.cs
+++ MyProject/Models/Veiculo.cs
@@ -35,23 +35,10 @@ namespace Automovel.Models
             Tipo = tipo;
         }
 
-        public void VenderVeiculos()
-        {
-            WriteLine("Deseja vender o veículo disponível? (s/n)");
-            var input = ReadLine();
-            if (input == "s")
-            {
-                WriteLine("Digite o ID(placa) do veículo: ");
-                var id = ReadLine();
-                WriteLine("Digite o CPF(somente números) do comprador: ");
-                var cpf = ReadLine();
-                WriteLine("Digite o valor do veículo: ");
-                var valor = ReadLine();
-                WriteLine("\n ---- Moto Vendida! ---- \n");
-                Console.ForegroundColor = ConsoleColor.Red;
-                WriteLine($"Placa: {id}, CPF comprador {cpf}, Valor: {valor} \n");
-                ResetColor();
-            }
-        }
+        public abstract void VenderVeiculos();
+
+        public void ListarInformacoes() { }
+
+        public void EditarInformacoes() { }
     }
 }
