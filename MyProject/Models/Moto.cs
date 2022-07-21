37632b1280f23b9ff13b86f2fb7e6f052a8c7540
Robson De Andrade <theairelibre@protonmail.com>
2022-07-21 11:21:22 -0300

    Add new field

--- MyProject/Models/Moto.cs
+++ MyProject/Models/Moto.cs
@@ -3,7 +3,8 @@ using static System.Console;
 namespace Automovel.Models
 {
     public class Moto : Veiculo
-    {
+    {   
+        public string brand = "Honda";
         public int rodas = 2;
         public Moto(
             string chassi,
@@ -13,22 +14,31 @@ namespace Automovel.Models
             double potencia,
             string cor,
             decimal valor,
-            ETipo tipo
+            EOpcao tipo
         ) : base(chassi, placa, modelo, data, potencia, cor, valor, tipo) { }
 
         public override void VenderVeiculos()
         {
             WriteLine("Deseja vender o veículo disponível? (s/n)");
+
             var input = ReadLine();
+            WriteLine("\n");
+
             if (input == "s")
             {
                 WriteLine("Digite o ID(placa) do veículo: ");
                 var id = ReadLine();
+                WriteLine("\n");
+
                 WriteLine("Digite o CPF(somente números) do comprador: ");
                 var cpf = ReadLine();
+                WriteLine("\n");
+
                 WriteLine("Digite o valor do veículo: ");
                 var valor = ReadLine();
-                WriteLine("\n ---- Moto Vendida ---- \n");
+                WriteLine("\n");
+
+                WriteLine("\n ---- Moto Vendida! ---- \n");
                 ForegroundColor = ConsoleColor.Red;
                 WriteLine($"Placa: {id}, CPF comprador {cpf}, Valor: {valor} \n");
                 ResetColor();
