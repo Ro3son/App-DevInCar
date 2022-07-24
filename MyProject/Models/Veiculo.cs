92035bc1c1c2ac39047b2b713debd210893ebdd3
Robson De Andrade <theairelibre@protonmail.com>
2022-07-23 21:45:35 -0300

    Remove parameters from the constructor

--- MyProject/Models/Veiculo.cs
+++ MyProject/Models/Veiculo.cs
@@ -3,44 +3,38 @@ namespace Automovel.Models
     public abstract class Veiculo
     {
         public string NumeroChassi { get; set; }
-        public string Placa { get; set; }
-        public string NomeModelo { get; set; }
+        public string? Placa { get; set; }
+        public string? NomeModelo { get; set; }
         public int DataFabricacao { get; set; }
         public double Potencia { get; set; }
-        public string Cor { get; set; }
+        public string? Cor { get; set; }
         public decimal Valor { get; set; }
-        public int CPF { get; set; }
+        public string? CPF { get; set; }
         public EMenu Tipo { get; set; }
+        public EStatus Status { get; set; }
 
-        public Veiculo(
-            string chassi,
-            string placa,
-            string modelo,
-            int data,
-            double potencia,
-            string cor,
-            decimal valor,
-            EMenu tipo
-        )
+        public Veiculo()
         {
             NumeroChassi = Convert.ToString(new Random().Next(17));
             Placa = Convert.ToString(new Random().Next(7));
-            NomeModelo = modelo;
-            DataFabricacao = data;
-            Potencia = potencia;
-            Cor = cor;
-            Valor = valor;
-            CPF = 0;
-            Tipo = tipo;
+            NomeModelo = NomeModelo;
+            DataFabricacao = DataFabricacao;
+            Potencia = Potencia;
+            Cor = Cor;
+            Valor = Valor;
+            CPF = CPF;
+            Tipo = Tipo;
+            Status = Status;
+
         }
 
         public abstract void VenderVeiculos();
-
-        public List<Veiculo> ListarInformacoes() 
+        
+        public List<Veiculo> veiculos = new List<Veiculo>();
+        public virtual List<Veiculo> ListarVeiculos() 
         { 
-            return new List<Veiculo>();
+            return veiculos;
         }
-
         public void EditarInformacoes() { }
     }
 }
