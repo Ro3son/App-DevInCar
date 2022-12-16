445e87e3848d69898251393768de662ad77db7c9
Robson De Andrade <theairelibre@protonmail.com>
2022-12-16 14:00:37 -0300

    feat: remove constructor

--- MyProject/Models/Veiculo.cs
+++ MyProject/Models/Veiculo.cs
@@ -4,7 +4,7 @@ namespace Automovel.Models
 {
     public abstract class Veiculo
     {
-        public string NumeroChassi { get; set; }
+        public string? NumeroChassi { get; set; }
         public string? Placa { get; set; }
         public string? NomeModelo { get; set; }
         public int DataFabricacao { get; set; }
@@ -15,32 +15,20 @@ namespace Automovel.Models
         public EMenu Tipo { get; set; }
         public EStatus Status { get; set; }
 
-        public Veiculo()
-        {
-            NumeroChassi = Convert.ToString(new Random().Next(17));
-            Placa = Convert.ToString(new Random().Next(7));
-            NomeModelo = NomeModelo;
-            DataFabricacao = DataFabricacao;
-            Potencia = Potencia;
-            Cor = Cor;
-            Valor = Valor;
-            CPF = CPF;
-            Tipo = Tipo;
-            Status = Status;
-        }
+        public Veiculo() { }
 
         public abstract void VenderVeiculos();
 
-        public List<Veiculo> veiculos = new List<Veiculo>();
+        public List<Veiculo> veiculos = new();
 
         public virtual List<Veiculo> ListarVeiculos()
         {
             return veiculos;
         }
 
-        public void EditarInformacoes() { }
+        public static void EditarInformacoes() { }
 
-        public void Menu()
+        public static void Menu()
         {
             ForegroundColor = ConsoleColor.DarkYellow;
             WriteLine(
